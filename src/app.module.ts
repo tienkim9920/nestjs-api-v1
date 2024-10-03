5f842bc85d35b8bd758a980eeefa9f8fe6d8112f
Tien Kim <tienkim9920@gmail.com>
2024-10-03 18:59:03 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,60 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './modules/products/product.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
-import { CarsEntity } from './entities/cars.entity';
-import { CategoriesEntity } from './entities/categories.entity';
-import { AccountsEntity } from './entities/accounts.entity';
-import { CategoryModule } from './modules/categories/category.module';
-import { CarModule } from './modules/cars/car.module';
-import { AuthModule } from './modules/auth/auth.module';
-
-import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from 'src/constant/constant';
-import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from './modules/auth/auth.guard';
-import { RolesGuard } from './modules/auth/roles.guard';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'nestjs-api-v1',
-      entities: [AccountsEntity, CarsEntity, CategoriesEntity],
-      synchronize: true,
-    }),
-    JwtModule.register({
-      global: true,
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: 900000 },
-    }),
-    ProductModule,
-    CategoryModule,
-    CarModule,
-    AuthModule,
-  ],
+  imports: [ProductModule],
   controllers: [AppController],
-  providers: [
-    AppService,
-    // jwt for global
-    {
-      provide: APP_GUARD,
-      useClass: AuthGuard,
-    },
-    // roles permission
-    {
-      provide: APP_GUARD,
-      useClass: RolesGuard,
-    }
-  ],
+  providers: [AppService],
 })
 export class AppModule {
-  constructor(private dataSource: DataSource) {
-
-  }
+  constructor() {}
 }
